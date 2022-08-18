155c169b78b5b4ba0fc1b75a0361e301f06864ca
rowena fonseka <rafonseka@gmail.com>
2022-08-18 00:07:24 -0400

    Create form input in Form component and add basic styles with scss.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Header from "./components/Header/Header";
 import Hero from "./components/Hero/Hero";
 import Highlight from "./components/Highlight/Highlight";
+import Form from "./components/Form/Form";
 import video from "./assets/data/video-details.json";
 import "./App.scss";
 
@@ -11,6 +12,7 @@ function App() {
       <Header />
       <Hero video={video} />
       <Highlight video={video} />
+      <Form />
     </div>
   );
 }
--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,2 +1,26 @@
 import "./Form.scss";
+import Mohan from "../../assets/Images/Mohan-muruge.jpg";
+import Comment from "../../assets/Icons/add_comment.svg";
 
+function Form (){
+
+    return (
+    <div className="form">
+        <div>
+        <img className="form__avatar" src={Mohan} alt="avatar" />
+        </div>
+        <div className="form__field">
+            <div>
+                <label className="form__label">join the conversation</label>
+                <textarea className="form__comment"name="commentField" placeholder="Add a new comment"></textarea>
+            </div>
+            <button className="form__btn"><img className="form__btn--icon" src={Comment} alt="comment icon"/>Add your Comment</button>
+               
+        </div>
+
+    </div>
+    );
+
+}
+
+export default Form;
