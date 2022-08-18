2c289feec0c7c02c4851c4cbcc8aa2f972244b89
rowena fonseka <rafonseka@gmail.com>
2022-08-17 20:20:31 -0400

    Create hero component, import video-detail json file and add as prop to Hero

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import Header from "./components/Header/Header";
+import Hero from "./components/Hero/Hero";
+import video from "./assets/data/video-details.json";
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
       <Header />
+      <Hero video={video} />
     </div>
   );
 }
