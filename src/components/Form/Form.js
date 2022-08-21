3a307bebc8b129518bf51005f2d95d97116c518e
rowena fonseka <rafonseka@gmail.com>
2022-08-21 11:56:10 -0400

    Fixed Comment button on Form component

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,6 +1,5 @@
 import "./Form.scss";
 import Mohan from "../../assets/Images/Mohan-muruge.jpg";
-import Comment from "../../assets/Icons/add_comment.svg";
 
 function Form() {
   return (
@@ -17,10 +16,7 @@ function Form() {
             placeholder="Add a new comment"
           ></textarea>
         </div>
-        <button className="form__btn">
-          <img className="form__btn--icon" src={Comment} alt="comment icon" />
-          Comment
-        </button>
+        <button className="form__btn">Comment</button>
       </div>
     </div>
   );
